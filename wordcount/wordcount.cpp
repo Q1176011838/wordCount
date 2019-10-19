93efef191a8004e5485f0408999138e9c158eddc
mmbhwhh <55627543+mmbhwhh@users.noreply.github.com>
2019-10-19 11:34:29 +0800

    Update wordcount.cpp

--- wordcount/wordcount.cpp
+++ wordcount/wordcount.cpp
@@ -1,5 +1,20 @@
 #include<iostream>
-using namespace std;
+using namespacvoid stat()
+{
+	char s[1000];
+	get.cin(s,1000);
+	for(int i=0;s[i]!='/0';i++)
+	{
+		if(s[i]<='Z'&&s[i]>='A')	s[i]+=32;
+		
+	}//��Сд 
+	int i=0;
+	
+	}
+
+
+e std;
+
 int main(int argc,char argv[]){
 	output();
 	return 0;
