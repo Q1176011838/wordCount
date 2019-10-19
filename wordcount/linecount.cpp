6bf730badf66d07f23adcb3376cec3572346e7df
lx509 <55038569+lx509@users.noreply.github.com>
2019-10-19 17:45:53 +0800

    Add files via upload

--- wordcount/linecount.cpp
+++ wordcount/linecount.cpp
@@ -1,48 +1,53 @@
 #include<string>
 #include<iostream>
 #include<stdio.h>
-#include<iostream>
 using namespace std;
 
 int lineCount(){
-	string str=inFile(); 
+	string str="\rfgsdfgsdg\rhgdfhh\rgfdsbgfd";  //inFile()调用输入函数返回值 
 		int n=str.size();
-		int linecount=1;
+		int linecount=0;
+		int temp=0;
+	if(str[0]!=' '&&str[0]!='\r'){
+		linecount++;
+	}
+	
+	else if(str[0]==' '){
 		for(int i=0;i<n;i++){
-			if(i!=0){
-				if(str[i]=='\r'){
-					for(int j=i+1;j<n;j++){
-						if(str[j]=='\r')
-						break;
-						if(str[j]!='\r'&&str[j]!=' '){
-							linecount++;
-							break;
-						}
-					}
-				} 
-			}
-		
-			/*else{
-				for(int j=i;j<n;j++){
-					if(str[0]=='\r')
-						break;
-					if(str[0]==' '){
-						for(int k=0;k<n;k++){
-							if(str[0]=='\r')
-								break;
-							if(str[k]!=' '){	
-								linecount++;
-								break;
-							}
-						}	
+			if(str[i]=='\r'){
+				//cout<<i<<"*"<<endl;
+				int j=i;
+				for(;j>0;j--){
+					//cout<<"______"<<endl;
+					if(str[j]!=' '&&str[j]!='\r'){
+						//cout<<j<<"$"<<endl;
+						linecount++;
+						goto stop;
 					}
 				}
-			}*/
-		}	
-	cout<<linecount;
+				if(j==0){
+					goto stop;
+				}
+			}
+		}
+	}
+
+	stop:for(int i=0;i<n;i++){
+		    if(str[i]=='\r'){
+				for(int j=i+1;j<n;j++){
+					if(str[j]=='\r')
+						break;
+					if(str[j]!='\r'&&str[j]!=' '){
+						linecount++;
+						break;
+					}
+				}
+			}
+	}
+	return linecount;
 }
 		
 int main(){
-		lineCount();
+	lineCount();
 	return 0;
 }
